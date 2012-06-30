4be7defe3f708af3f0628fc71139ade5d9d138fc
Alejandro Martínez <yosoyzenitram@gmail.com>
2012-06-30 15:32:50 +0200

    Order

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -33,7 +33,23 @@ class OrderController < ApplicationController
   end
 
   def choose
+	order_id = params[:id]
+	@order = Order.find(order_id)
+  end
 
+  def add
+	order_id = params[:id]
+	user_id = session[:user_id]
+	product_id = params[:product_id]
+	quantity = params[:quantity]
+
+	order = Order.find(order_id)
+	user = order.users.find(user_id)
+	product = order.offer.product.find(product_id)
+	item = user.item.new
+	item.product = product
+	item.quantity = quantity
+	item.save
   end
 
 end
